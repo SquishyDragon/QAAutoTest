c96a6dc7e5f7fba775ff34a03f999f2b11f02379
Nathan Treadaway <natetread@gmail.com>
2025-10-24 09:12:47 -0400

    working db routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,47 @@
-const express = require("express");
+import express from "express";
 const app = express();
+import Database from "better-sqlite3";
+import cors from "cors";
+
 const port = 3000;
 
-// Define a simple route
-app.get("/", (req, res) => {
-  res.send("Hello World from Express!");
+app.use(cors());
+app.use(express.json());
+
+const db = new Database("results.db");
+
+db.prepare(
+  `
+  CREATE TABLE IF NOT EXISTS results (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    test_name TEXT,
+    status TEXT,
+    duration INTEGER,
+    timestamp TEXT
+  )
+`
+).run();
+
+// Get Results
+app.get("/resutls", (req, res) => {
+  const rows = db.prepare("SELECT * FROM results ORDER BY id DESC").all();
+  res.json(rows);
 });
 
+app.post("/resutls", (req, res) => {
+  const data = req.body;
+  const stmt = db.prepare(`
+  INSERT INTO results (test_name, status, duration, timestamp)
+  VALUES (?, ?, ?, ?)
+`);
+  const info = stmt.run(
+    data.test_name,
+    data.status,
+    data.duration,
+    new Date().toISOString()
+  );
+  res.send("done");
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
